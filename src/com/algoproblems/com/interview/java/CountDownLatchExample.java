316db8f9f5079814a53b01d5cc6b1ffa926890b5
Alok Dubey <alok.dwivedi0609@gmail.com>
2018-08-19 15:18:40 +0530

    Updated CountDownLatch example

--- src/com/algoproblems/com/interview/java/CountDownLatchExample.java
+++ src/com/algoproblems/com/interview/java/CountDownLatchExample.java
@@ -3,16 +3,15 @@ package com.interview.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class CountDownLatchExample {
-    private final CyclicBarrier cyclicBarrier;
+    private final CountDownLatch countDownLatch;
 
-    public CountDownLatchExample(int n, Runnable barrierTask) {
-        this.cyclicBarrier = new CyclicBarrier(n, barrierTask);
+    public CountDownLatchExample(int n) {
+        this.countDownLatch = new CountDownLatch(n);
     }
 
     private ExecutorService executorService = Executors.newFixedThreadPool(5);
@@ -20,24 +19,40 @@ public class CountDownLatchExample {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(1, 2, 3, 4);
         List<Integer> squared = new ArrayList<>();
-        CountDownLatchExample task = new CountDownLatchExample(nums.size(), () -> {
-            System.out.println("Inside barrier task!");
-            System.out.println(squared.stream().reduce((a, b) -> a + b).get());
-        });
+        CountDownLatchExample task = new CountDownLatchExample(nums.size());
+
         Arrays.asList(1, 2, 3, 4).forEach(num -> {
             task.executorService.execute(() -> {
                 System.out.println("num:: " + num + " " + Thread.currentThread().getName());
-                try {
-                    squared.add(num * num);
-                    task.cyclicBarrier.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (BrokenBarrierException e) {
-                    e.printStackTrace();
-                }
+                squared.add(num * num);
+                task.countDownLatch.countDown();
             });
         });
-        System.out.println("Thread Name: "+Thread.currentThread().getName());
+
+        try {
+            System.out.println("Count before await: " + task.countDownLatch.getCount());
+            task.countDownLatch.await();
+            System.out.println("Count after await: " + task.countDownLatch.getCount());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(squared.stream().reduce((a, b) -> a + b).get());
+        System.out.println("Thread Name: " + Thread.currentThread().getName());
         task.executorService.shutdown();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
